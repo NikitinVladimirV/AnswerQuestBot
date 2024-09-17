e7d9db51d76ea809cd76bac655a314dfe5a6ff27
Vladimir Nikitin <vreservoirv@rambler.ru>
2024-09-17 17:52:34 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const TelegramApi = require("node-telegram-bot-api");
+const {gameOptions, againOptions} = require('./options.js')
+
+const token = "7255500226:AAGnVMVD7OijgafI8E8yulwztN5ZCFFe3qU";
+const bot = new TelegramApi(token, {polling: true});
+const chats = {};
+
+const startGame = async (chatId) => {
+  await bot.sendMessage(chatId, "Сейчас я загадаю цифру от 0 до 9, а ты попробуй ее угадать)");
+  
+  const randomNumber = Math.floor(Math.random() * 10);
+  chats[chatId] = randomNumber;
+
+  await bot.sendMessage(chatId, "Отгадывай!", gameOptions);
+};
+
+const start = () => {
+  bot.setMyCommands([
+    {command: "/start", description: "Начальное приветствие"},
+    {command: "/info", description: "Информация о себе"},
+    {command: "/game", description: "Играть"},
+  ])
+
+  bot.on("message", async msg => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+    const userData = msg.from;
+
+    if (text === '/start') {
+      // await bot.sendSticker(chatId, "https://tgram.ru/wiki/stickers/img/Privet_1/png/16.png");
+      return await bot.sendMessage(chatId, `${userData.first_name}, привет)! Очень рад познакомиться!`);
+    };
+
+    if (text === "/info") {
+      // await bot.sendSticker(chatId, "https://tgram.ru/wiki/stickers/img/Privet_1/png/16.png");
+      return await bot.sendMessage(chatId, `Тебя зовут ${userData.first_name} ${userData.last_name ?? ''})`);
+    };
+
+    if (text === "/game") {
+      return startGame(chatId);
+    };
+
+    return await bot.sendMessage(chatId, "Что-то я тебя не очень понял, ты точно не хотел меня обидеть?");
+    
+  });
+
+  bot.on('callback_query', async msg => {
+    const data = msg.data;
+    const chatId = msg.message.chat.id;
+
+    if (data === "/again") return startGame(chatId);
+
+    console.log(+data, chats[chatId]);
+
+    if (+data === chats[chatId]) {
+      return await bot.sendMessage(chatId, `Ура! Ты угадал, это цифра ${chats[chatId]}`, againOptions);
+    } else {
+      return await bot.sendMessage(chatId, `Неа, ты не угадал, это цифра ${chats[chatId]}...`, againOptions);
+    };
+
+  });
+};
+
+start();
